695f42b15c2e4232b562b4ecfbbe5ab9666eb6d8
gnishida <gnishida@purdue.edu>
2018-07-17 15:07:05 -0400

    Removed unnecessary lines.

--- ShapeFitting/ShapeFit.cpp
+++ ShapeFitting/ShapeFit.cpp
@@ -30,38 +30,6 @@ std::vector<cv::Point2f> ShapeFit::fit(const std::vector<cv::Point2f>& polygon, 
 	}
 
 	try {
-		/*
-		column_vector starting_point(8);
-		starting_point(0) = 0.2;
-		starting_point(1) = 0.2;
-		starting_point(2) = 0.8;
-		starting_point(3) = 0.2;
-		starting_point(4) = 0.8;
-		starting_point(5) = 0.8;
-		starting_point(6) = 0.2;
-		starting_point(7) = 0.8;
-		*/
-
-		/*
-		column_vector starting_point(16);
-		starting_point(0) = 0.3;
-		starting_point(1) = 0.1;
-		starting_point(2) = 0.7;
-		starting_point(3) = 0.1;
-		starting_point(4) = 0.9;
-		starting_point(5) = 0.3;
-		starting_point(6) = 0.9;
-		starting_point(7) = 0.7;
-		starting_point(8) = 0.7;
-		starting_point(9) = 0.9;
-		starting_point(10) = 0.3;
-		starting_point(11) = 0.9;
-		starting_point(12) = 0.1;
-		starting_point(13) = 0.7;
-		starting_point(14) = 0.1;
-		starting_point(15) = 0.3;
-		*/
-
 		BFGSSolver solver(normalized_polygon);
 		//find_max_using_approximate_derivatives(dlib::bfgs_search_strategy(), dlib::objective_delta_stop_strategy(1e-7),	solver, starting_point, 1, 0.0001);
 		find_min_using_approximate_derivatives(dlib::bfgs_search_strategy(), dlib::objective_delta_stop_strategy(1e-7), solver, starting_point, 0, 0.0001);
